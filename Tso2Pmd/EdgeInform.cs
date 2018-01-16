ddfb0445217ac3fe66883c1b736b6fb3e59473cc
nomeu <n.nomeu@gmail.com>
2018-01-16 15:21:08 +0900

    import tso2pmx ver 0.4.0.1

---
+++ Tso2Pmd/EdgeInform.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Tso2Pmd
+{
+    class EdgeInform : IDisposable
+    {
+        static string edgefile = "edge.ini";
+        static Encoding encoding = Encoding.GetEncoding("Shift_JIS");
+        Dictionary<String, Boolean> edgeflag;
+
+        // コンストラクタ：フラグを辞書に読み込む.
+        public EdgeInform()
+        {
+            using (StreamReader sr = new StreamReader(edgefile, encoding))
+            {
+                edgeflag = new Dictionary<string, bool>();
+                string line = null;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    edgeflag.Add(line,true);
+                }
+            }
+        }
+
+        // 指定されたテクニックがエッジを持っているか返す.
+        public Boolean hasedge(string technique)
+        {
+            return edgeflag.ContainsKey(technique);
+        }
+
+        public void Dispose()
+        {
+            edgeflag = null;
+        }
+    }
+}
